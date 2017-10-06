357c39d75eead95ef15b59f908bfb9fb0d2508c6
bloudman <adriencom@msn.com>
2017-10-06 10:14:19 +0200

    remove ligne inutile

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var datas = new Map();
-
 
 //Roomba
 roomba = new Robot("/dev/cu.usbserial-A700eXK6");
