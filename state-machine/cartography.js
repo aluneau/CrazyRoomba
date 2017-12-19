ad194612bed79f2e15fa1c856c8a7ee25a172ca1
Nirvador <x.herwyn@laposte.net>
2017-12-19 22:03:27 +0100

    cartography final

--- state-machine/cartography.js
+++ state-machine/cartography.js
@@ -1,188 +1,163 @@
-var mqtt = require("mqtt");
+/* Zone de declaration des variables */
 
-var client = mqtt.connect("mqtt://localhost");
+var mqtt, client, flag, bump, pDistance, turnAngle, messageGlobal, angle, distance,
+  previousPoint, point, StateMachine, strategyNumber, machineEtat;
 
-var flag = 0;
+/* Fin de la zone de declaration */
 
-var bump = 0;
+/* Zone d'initialisation des variables */
+mqtt = require('mqtt');
+client = mqtt.connect('mqtt://localhost');
+StateMachine = require('javascript-state-machine');
 
-var pDistance = 0;
+flag = false;
+bump = false;
 
-var turnAngle = -45;
-
-var messageGlobal=null;
-
-function convertToRadian(degrees) {
-    var pi = Math.PI;
-    return degrees * (pi / 180);
-}
-
+pDistance = 0;
+turnAngle = (-45);
+messageGlobal = null;
+angle = 0;
+distance = 0;
+strategyNumber = 0;
 
 class Point {
-    constructor() {
-        this.x = 0;
-        this.y = 0;
-    }
+  constructor () {
+    this.x = 0;
+    this.y = 0;
+  }
 }
 
-var pointN_1 = new Point();
+previousPoint = new Point();
+point = new Point();
 
-var angle = 0;
-var distance = 0;
+/* Fin de la zone d'initialisation des variables */
 
-setInterval(function () {
-    flag = 0;
-}, 1500);
+/* Zone de declaration et d'initialisation de la machine d'etat */
 
-
-
-
-
-var StateMachine = require('javascript-state-machine');
-var mqtt = require("mqtt");
-var strategyNumber = 0;
-
-var machineEtat = new StateMachine({
-    init: 'pause',
-    transitions:
-        [
+machineEtat = new StateMachine({
+  init: 'pause',
+  transitions:
+  [
 
             { name: 'datas', from: 'pause', to: 'datas' },
             { name: 'angle', from: 'pause', to: 'angle' },
             { name: 'distance', from: 'pause', to: 'distance' },
-            { name: 'goto', from: '*', to: function(state){return state;} }
+            { name: 'pause', from: '*', to: 'pause' },
+            { name: 'goto', from: '*', to: function (state) { return state; } }
 
+  ],
+  methods:
+  {
+    onEnterPause: function () { },
 
-        ],
-    methods:
-        {
-            onEnterPause: function () { },
+    onEnterDatas: function (/* /*message/**/) {
+// console.log("onenterdatas");
+      let datas, BumpsAndWheelDrops;
 
-            onEnterDatas: function (/* /*message/**/) {
-//console.log("onenterdatas");
-                let datas = JSON.parse(messageGlobal);
-                let BumpsAndWheelDrops = null;
-                for (let i = 0; i < datas.length; i++) {
-                    if (datas[i].name == "BumpsAndWheelDrops") {
-                        BumpsAndWheelDrops = datas[i].value;
-                    }
-                }
+      datas = JSON.parse(messageGlobal);
+      BumpsAndWheelDrops = null;
 
-                if (BumpsAndWheelDrops != undefined && BumpsAndWheelDrops > 0 && flag == 0) {
-                    //client.publish("/roomba/getDistance");
-
-                    if (BumpsAndWheelDrops == 1) {
-                        client.publish("/roomba/turn", JSON.stringify(-turnAngle));
-                        //angle-=turnAngle;
-                    }
-                    if (BumpsAndWheelDrops == 2) {
-                        client.publish("/roomba/turn", JSON.stringify(turnAngle));
-                        //angle+=turnAngle;
-                    }
-                    if (BumpsAndWheelDrops == 3) {
-                        client.publish("/roomba/turn", JSON.stringify(100));
-                        //angle+=100;
-                    }
-
-                    bump = 1;
-                    flag = 1;
-                }
-            },
-            onEnterAngle: function () {
-                angle = JSON.parse(messageGlobal);
-                
-            },
-            onEnterDistance: function () {
-                let retrivedDistance = JSON.parse(messageGlobal);
-
-                let distance = retrivedDistance - pDistance;
-                var point = new Point();
-
-                point.x = pointN_1.x + distance * Math.cos(convertToRadian(angle));
-                point.y = pointN_1.y + distance * Math.sin(convertToRadian(angle));
-
-                let toSend = "{\"x\":" + point.x / 10 + ", \"y\":" + point.y / 10 + ", \"bump\": " + bump + ", \"angle\":" + angle + "}";
-                bump = 0;
-                //console.log(toSend);
-                //console.log(toSend);
-                client.publish("/roomba/points", toSend);
-
-                angle %= 360;
-                //console.log("Distance: ", retrivedDistance - pDistance);
-                pDistance = retrivedDistance;
-
-                pointN_1 = point;
-                client.publish("/roomba/driveDirect", JSON.stringify([100, 100]));
-
-            }
-
+      for (let i = 0; i < datas.length; i++) {
+        if (datas[i].name === 'BumpsAndWheelDrops') {
+          BumpsAndWheelDrops = datas[i].value;
         }
+      }
 
-})
+      if (BumpsAndWheelDrops !== undefined && BumpsAndWheelDrops > 0 && flag === false) {
+        let angleDefault = 0;
 
-client.on("message", function choixStrategy(topic,message){
-    if (topic == "/roomba/strategy") {
-        strategyNumber = JSON.parse(message);
-        console.log(strategyNumber );        
+        switch (BumpsAndWheelDrops) {
+          case 1: angleDefault = ((-1) * turnAngle); break;
+          case 2: angleDefault = turnAngle; break;
+          case 3: angleDefault = 100; break;
+        }
+        client.publish('/roomba/turn', JSON.stringify(angleDefault));
+
+        bump = true;
+        flag = true;
+      }
+    },
+    onEnterAngle: function () {
+      angle = JSON.parse(messageGlobal);
+    },
+    onEnterDistance: function () {
+      let retrivedDistance, toSend;
+
+      retrivedDistance = JSON.parse(messageGlobal);
+      distance = retrivedDistance - pDistance;
+
+      point.x = previousPoint.x + distance * Math.cos(convertToRadian(angle));
+      point.y = previousPoint.y + distance * Math.sin(convertToRadian(angle));
+
+      toSend = '{"x":' + point.x / 10 + ', "y":' + point.y / 10 + ', "bump": ' + bump + ', "angle":' + angle + '}';
+      client.publish('/roomba/points', toSend);
+
+      angle = angle % 360;
+      pDistance = retrivedDistance;
+      previousPoint = point;
+
+      bump = false;
+
+      client.publish('/roomba/driveDirect', JSON.stringify([100, 100]));
     }
-    switch (strategyNumber) {
-        case 1:
-            bumpAndTurn(topic, message);
-            break;
-        case 0:
-            resetRoomba(topic, message);
-            break;
-    }
-
-    if(topic == "/roomba/reset"){
-        resetRoomba(topic, message);
-    }
-    });
-            
-
-client.on("connect", function () {
-    console.log("MQTT Ok");
-    client.subscribe("/roomba/datas");
-    client.subscribe("/roomba/distance");
-    client.subscribe("/roomba/angle");
-    client.subscribe("/roomba/reset");
-    client.subscribe("/roomba/strategy");
-    //client.publish("/roomba/driveDirect", JSON.stringify([100, 100]));
-
-    //client.publish("/roomba/sendCommand", JSON.stringify([132,145,1,44,1,44,158,5,137,0,127,0,1,157,0,90,137,0,0,0,0,148,1,19]));
-    setInterval(function () {
-        client.publish("/roomba/getDistance");
-        client.publish("/roomba/getPhoneAngle");
-    }, 200);
-
+  }
 });
 
+/* Fin de la zone de la declaration et d'initialisation de la machine d'etat */
 
-function resetRoomba(topicReset, messageReset){
-    client.publish("/roomba/driveDirect", JSON.stringify([0,0]));
-    messageGlobal = null;
-    distance = 0;
-    pDistance = 0;
+setInterval(function () {
+  flag = false;
+}, 1500);
+
+function convertToRadian (degrees) {
+  let pi = Math.PI;
+  return degrees * (pi / 180);
 }
 
-function bumpAndTurn(topicLocal, messageLocal){
-    if (topicLocal == "/roomba/angle") {
-        //console.log("angle1");
-        messageGlobal = messageLocal;
-        machineEtat.angle();
-        machineEtat.goto("pause");
-    }
-    if (topicLocal == "/roomba/datas") {
-        //console.log("datas");
-        messageGlobal = messageLocal;
-        machineEtat.datas();
-        machineEtat.goto("pause");
-    }
-    if (topicLocal == "/roomba/distance") {
+client.on('message', function choixStrategy (topic, message) {
+  if (topic === '/roomba/strategy') {
+    strategyNumber = JSON.parse(message);
+    console.log(strategyNumber);
+  }
+  switch (strategyNumber) {
+    case 1: bumpAndTurn(topic, message); break;
+    case 0: resetRoomba(); break;
+  }
 
-        messageGlobal = messageLocal;
-        machineEtat.distance();
-        machineEtat.goto("pause");
-        //console.log(JSON.parse(message));
-    }
+  /* if (topic === '/roomba/reset') {
+    resetRoomba();
+  } */
+});
+
+client.on('connect', function () {
+  console.log('MQTT Ok');
+  client.subscribe('/roomba/datas');
+  client.subscribe('/roomba/distance');
+  client.subscribe('/roomba/angle');
+  client.subscribe('/roomba/reset');
+  client.subscribe('/roomba/strategy');
+    // client.publish("/roomba/driveDirect", JSON.stringify([100, 100]));
+
+    // client.publish("/roomba/sendCommand", JSON.stringify([132,145,1,44,1,44,158,5,137,0,127,0,1,157,0,90,137,0,0,0,0,148,1,19]));
+  setInterval(function () {
+    client.publish('/roomba/getDistance');
+    client.publish('/roomba/getPhoneAngle');
+  }, 200);
+});
+
+function resetRoomba () {
+  client.publish('/roomba/driveDirect', JSON.stringify([0, 0]));
+  messageGlobal = null;
+  distance = 0;
+  pDistance = 0;
+}
+
+function bumpAndTurn (topicLocal, messageLocal) {
+  messageGlobal = messageLocal;
+  switch (topicLocal) {
+    case '/roomba/angle': machineEtat.angle(); break;
+    case '/roomba/datas': machineEtat.datas(); break;
+    case '/roomba/distance': machineEtat.distance(); break;
+  }
+  machineEtat.pause();
 }
